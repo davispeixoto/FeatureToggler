5b017ba729df801a7f006c6861b96fc292e9dc66
Davis Peixoto <davis.peixoto@rentcars.com>
2015-08-17 18:03:15 -0300

    updates!

--- src/FeatureToggler.php
+++ src/FeatureToggler.php
@@ -8,6 +8,8 @@
 
 namespace Davispeixoto\FeatureToggler;
 
+use Noodlehaus\Config;
+
 class FeatureToggler
 {
     /**
@@ -18,31 +20,14 @@ class FeatureToggler
     public function __construct($configFile)
     {
         if (is_readable($configFile)) {
-            $this->config = include($configFile);
+            $this->config = Config::load($configFile);
         } else {
             throw new FeatureTogglerException('Config file does not exists, or cannot be read');
         }
     }
 
-    public function isEnabled()
+    public function isEnabled($key, $defaultValue = false)
     {
-        $args = func_get_args();
-        return $this->searchKey($args, $this->config);
-    }
-
-    private function searchKey($lookForIt, $context)
-    {
-        $key = array_shift($lookForIt);
-        if (array_key_exists($key, $context)) {
-            if (is_bool($context[$key])) {
-                return $context[$key];
-            } elseif (is_array($context[$key])) {
-                return $this->searchKey($lookForIt, $context[$key]);
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
+        return $this->config->get($key, $defaultValue);
     }
 }
